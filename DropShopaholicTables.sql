041cd5f99b2c845f2846f52373804f88aa98a857
Jasmine Lao <103599216+jsmnlao@users.noreply.github.com>
2023-08-02 10:16:03 -0700

    Add files via upload

---
+++ DropShopaholicTables.sql
@@ -0,0 +1,15 @@
+USE Shopaholic;
+
+DROP TABLE CartProducts;
+DROP TABLE Cart;
+DROP TABLE ReviewsTextualContext;
+DROP TABLE Reviews;
+DROP TABLE MerchantOrders;
+DROP TABLE Orders;
+DROP TABLE MerchantProducts;
+DROP TABLE Products;
+DROP TABLE Merchants;
+DROP TABLE MemberUsersFriends;
+DROP TABLE GuestUsers;
+DROP TABLE MemberUsers;
+DROP TABLE Admin;
